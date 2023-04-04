06124cef142ff189ee28aaeaa072292d13004beb
stylessh <stylesshjs@gmail.com>
2023-04-04 11:32:00 -0400

    feat: navbar menu added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
-import { EthereumIcon } from "@/ui/icons";
 import Link from "next/link";
+import { EthereumIcon } from "@/ui/icons";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+
+import { Menu } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   return (
@@ -9,18 +12,64 @@ const Navbar = () => {
         BLOCKHACK
       </Link>
 
-      <nav className="absolute top-0 left-1/2 -translate-x-1/2 bg-primary-500 text-black w-32 h-12 grid place-items-center rounded-b-3xl rounded-t-sm cursor-pointer">
-        <Bars3Icon className="w-8 h-8" />
-      </nav>
+      <Menu
+        as="nav"
+        className="absolute top-0 z-50 flex flex-col items-center -translate-x-1/2 outline-none left-1/2"
+      >
+        <Menu.Items className="flex flex-col text-center text-black divide-y divide-black shadow-2xl w-72 top-12 bg-primary-500 rounded-2xl">
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              Courses
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              About
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              Blog
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              Podcast
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              Account
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="#" className="py-2 text-2xl">
+              Support
+            </Link>
+          </Menu.Item>
+        </Menu.Items>
+
+        <Menu.Button className="flex items-center justify-center w-32 h-12 text-black shadow-2xl bg-primary-500 rounded-b-2xl">
+          {({ open }) => (
+            <>
+              {open ? (
+                <XMarkIcon className="w-8 h-8" />
+              ) : (
+                <Bars3Icon className="w-8 h-8" />
+              )}
+            </>
+          )}
+        </Menu.Button>
+      </Menu>
 
       {/* button */}
       <div className="flex items-center space-x-2">
-        <button className="text-white border border-white rounded-full py-1 px-8 text-2xl inline-flex items-center">
+        <button className="inline-flex items-center px-8 py-1 text-2xl text-white border border-white rounded-full">
           <EthereumIcon className="mr-2" />
           ETHEREUM
         </button>
 
-        <button className="text-primary-500 border border-primary-500 rounded-full py-1 px-8 text-2xl">
+        <button className="px-8 py-1 text-2xl border rounded-full text-primary-500 border-primary-500">
           LOGIN
         </button>
       </div>
